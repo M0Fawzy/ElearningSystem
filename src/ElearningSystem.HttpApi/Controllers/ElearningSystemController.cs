1e59f0089a54606c4f6d0ef460dc5bf3b98ccae1
Mohamed Fawzy <64755201+M0Fawzy@users.noreply.github.com>
2026-08-08 01:54:55 +0300

    Add files via upload

---
+++ src/ElearningSystem.HttpApi/Controllers/ElearningSystemController.cs
@@ -0,0 +1,14 @@
+﻿using ElearningSystem.Localization;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace ElearningSystem.Controllers;
+
+/* Inherit your controllers from this class.
+ */
+public abstract class ElearningSystemController : AbpControllerBase
+{
+    protected ElearningSystemController()
+    {
+        LocalizationResource = typeof(ElearningSystemResource);
+    }
+}
