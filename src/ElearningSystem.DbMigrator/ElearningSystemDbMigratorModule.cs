63079d1128dffbb2485a75c8f4b9b9ecaac4bae8
Mohamed Fawzy <64755201+M0Fawzy@users.noreply.github.com>
2026-08-08 01:58:30 +0300

    Add files via upload

---
+++ src/ElearningSystem.DbMigrator/ElearningSystemDbMigratorModule.cs
@@ -0,0 +1,14 @@
+﻿using ElearningSystem.EntityFrameworkCore;
+using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+
+namespace ElearningSystem.DbMigrator;
+
+[DependsOn(
+    typeof(AbpAutofacModule),
+    typeof(ElearningSystemEntityFrameworkCoreModule),
+    typeof(ElearningSystemApplicationContractsModule)
+    )]
+public class ElearningSystemDbMigratorModule : AbpModule
+{
+}
